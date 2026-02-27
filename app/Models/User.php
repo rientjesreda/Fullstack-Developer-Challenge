74da5b5f9de4b27454b16754d1437763dbd442ae
reda <redamargai6@gmail.com>
2026-02-27 14:17:32 +0100

    les 5

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * De velden die massaal ingevuld mogen worden.
      *
      * @var list<string>
      */
@@ -24,7 +24,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * De velden die verborgen moeten zijn bij serialisatie.
      *
      * @var list<string>
      */
@@ -34,7 +34,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Typecasts voor bepaalde velden.
      *
      * @return array<string, string>
      */
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\AuthController;
  
 // ...
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    // ...
+});
+ 
+Route::post('/auth/register', [AuthController::class, 'register']);
  Route::apiResource('transactions', TransactionController::class);
 Route::apiResource('categories', CategoryController::class);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// ...
+ 
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('transactions', TransactionController::class);
+
+    Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
+});
