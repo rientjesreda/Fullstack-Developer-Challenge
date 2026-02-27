36fb98963997b698f7bf963dd5c33164808eefce
reda <redamargai6@gmail.com>
2026-02-27 13:40:28 +0100

    les 2

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,18 +1,10 @@
-<?php
-
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Models\Category;
  
-// ...
- 
-public function index()
-{
-    return Category::all();
-}
-
 class CategoryController extends Controller
 {
     /**
@@ -20,40 +12,44 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return CategoryResource::collection(Category::all());
     }
-
+ 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        return new CategoryResource(Category::create($request->validated()));
     }
-
+ 
     /**
      * Display the specified resource.
      */
     public function show(Category $category)
     {
-        //
+        return new CategoryResource($category);
     }
-
+ 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update($request->validated());
+ 
+        return new CategoryResource($category);
     }
-
+ 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+ 
+        return response()->noContent();
     }
-
-    
 }
